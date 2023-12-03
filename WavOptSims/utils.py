80962185a4231154863cb60461782f684850e1b1
Sagnik Ghosh <sagnikg@andrew.cmu.edu>
2023-12-03 11:01:20 -0500

    Added slm_ramp

--- WavOptSims/utils.py
+++ WavOptSims/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def lens(u1, x1, y1, f, lmbd=500e-9):
+def lens_fourier(u1, x1, y1, f, lmbd=500e-9):
     """
     Function to transform field from input plane-1
     to focal plane-2
@@ -47,5 +47,40 @@ def lens(u1, x1, y1, f, lmbd=500e-9):
     u2 = np.fft.fft2(u1*phase_fact)
     u2 = u2*(delta1*delta1/(lmbd*f)) * \
         np.exp(-1j*2*np.pi*(x2+y2)*c1*(1/(lmbd*f)))
+
+    return u2, x2, y2
+
+def slm_ramp(u1, x1, y1, vx, vy, lmbd=500e-9):
+    """
+    Function to simulate SLM with linear ramp
+
+    Parameters:
+    u1 : np.nddarray
+    Input field
+    x1 : np.ndarray
+    x coords of input field, uniformly distributed
+    y1 : np.ndarray
+    y coords of input field, uniformly distributed
+    vx : float
+    slope of ramp in x-direction
+    vy : float
+    slope of ramp in y-direction
+
+    Returns:
+    u2 : np.nddarray
+    Output field
+    x2 : np.ndarray
+    x coords of output field, uniformly distributed
+    y2 : np.ndarray
+    y coords of output field, uniformly distributed
+    """
+
+    # Multiply by phase ramp
+    ramp = np.exp(-1j*2*(np.pi/lmbd)*(x1*vx+y1*vy))
+    u2 = u1*ramp
+
+    # x and y coords
+    x2 = x1
+    y2 = y1
 
     return u2, x2, y2
