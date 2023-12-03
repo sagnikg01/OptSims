bf2a7c7c6e90ad81309c98c82f5a53a5dcbf0c34
Sagnik Ghosh <sagnikg@andrew.cmu.edu>
2023-12-03 09:48:34 -0500

    Clean up

--- WavOptSims/utils.py
+++ WavOptSims/utils.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 def lens(u1, x1, y1, f, lmbd=500e-9):
     """
     Function to transform field from input plane-1
     to focal plane-2
-    
+
     Parameters:
     u1 : np.nddarray
     Input field
@@ -14,7 +15,7 @@ def lens(u1, x1, y1, f, lmbd=500e-9):
     y coords of input field, uniformly distributed
     f : float
     focal length
-    
+
     Returns:
     u2 : np.nddarray
     Output field
@@ -23,14 +24,16 @@ def lens(u1, x1, y1, f, lmbd=500e-9):
     y2 : np.ndarray
     y coords of output field, uniformly distributed
     """
-    
+
     # Intial params
-    N = u1.shape[0]
-    W1 = (x1[0,1]-x1[0,0])*N
-    W2 = N*lmbd*f/W1
-    delta1 = W1/N
-    delta2 = W2/N
-    
+    N = u1.shape[0]  # Number of samples
+    W1 = (x1[0, 1]-x1[0, 0])*N  # Input width
+    W2 = N*lmbd*f/W1  # Output Width
+    delta1 = W1/N  # Input spacing
+    delta2 = W2/N  # Output Spacing
+    c1 = -W1/2 + delta1/2  # Bias 1
+    c2 = -W2/2 + delta2/2  # Bias 2
+
     # Compute meshgrids
     nx = np.arange(N)
     ny = np.arange(N)
@@ -38,12 +41,11 @@ def lens(u1, x1, y1, f, lmbd=500e-9):
     y2 = -W2/2 + delta2/2 + ny*delta2
     nx, ny = np.meshgrid(nx, ny)
     x2, y2 = np.meshgrid(x2, y2)
-    c1 = -W1/2 + delta1/2
-    c2 = -W2/2 + delta2/2
-    
+
     # Compute fourier transform
     phase_fact = np.exp(-1j*(2*np.pi)*(1/(lmbd*f))*(nx+ny)*delta1*c2)
     u2 = np.fft.fft2(u1*phase_fact)
-    u2 = u2*(delta1*delta1/(lmbd*f))*np.exp(-1j*2*np.pi*(x2+y2)*c1*(1/(lmbd*f)))
-    
+    u2 = u2*(delta1*delta1/(lmbd*f)) * \
+        np.exp(-1j*2*np.pi*(x2+y2)*c1*(1/(lmbd*f)))
+
     return u2, x2, y2
